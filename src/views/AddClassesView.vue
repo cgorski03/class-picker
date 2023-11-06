3c2923bb5334ecbc8c40607535cc6f694c7cd556
jak19018 <joshua.a.kaplan@uconn.edu>
2023-11-06 11:10:02 -0500

    Making search bar with css in AddClassView

--- src/views/AddClassesView.vue
+++ src/views/AddClassesView.vue
@@ -10,7 +10,17 @@
             <h2>Add Classes</h2>
           </div>
           <div>
-            <ClassTable/>
+            <form class = "searchbar">
+              <div id = "searchfield">
+                <input type="text" placeholder="Seacrh by class name" id="className" v-model="className" />
+              </div>
+              <div id = "searchbutton">
+                <button v-on:click.prevent="search"></button>
+              </div>
+            </form>
+          </div>
+          <div>
+            <!--<ClassTable/>-->
           </div>
         </div>
       </div>
@@ -20,8 +30,15 @@
   
   <script setup>
   import MainMenu from "../components/MainMenu.vue";
-  import ClassTable from "../components/AddClassTable";
-  
+  //import ClassTable from "../components/AddClassTable.vue";
+  import { ref } from "vue"
+
+  const className = ref("");
+
+  const search = () => {
+    //Implent fetch to get list of classes corresponding to name
+  }
+
   </script>
   <style scoped>
   .home {
@@ -36,12 +53,6 @@
     background-color: #151E3D;
     color: white;
     padding: 25px;
-  }
-  
-  img {
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
   }
   
   .menuContent {  
@@ -59,4 +70,57 @@
     flex-direction:row;  
   }
   
+
+  .searchbar {
+    background-color: #151E3D;
+    margin-top: 30px;
+    margin-bottom: 30px;
+    margin-left: 60px;
+    margin-right: 60px;
+    padding: 30px;
+    width: 90%;
+    display: flex;
+    flex-direction: row;
+  }
+  .searchbar #searchfield{
+    margin-left: 120px;
+    width: 75%;
+  }
+
+  
+  .searchbar input{
+    width: 100%;
+    margin-left: auto;
+    margin-right: auto;
+    font-size: 30px;
+    display: block;
+  }
+
+  .searchbar input::placeholder{
+    text-align: center;
+    color: black;
+    font-weight: 500;
+  }
+  .searchbar input:hover{
+    background-color: lightgray;
+
+  }
+
+  .searchbar button{
+    border-radius: 15%;
+    background: url("../assets/images/search.webp") no-repeat scroll 0 0 transparent;
+    background-size: 50px;
+    color: #000000;
+    cursor: pointer;
+    padding: 25px 25px;
+    border: 1px white;
+  }
+
+  .searchbar button:hover{
+    background-color: grey;
+  }
+
+  .searchbar button:active{
+    background-color: darkgray;
+  }
   </style>
