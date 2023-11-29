71a3e1a893df5e869aa816dae21b58d1bcf115b8
Colin Gorski <colingorski@Colins-MacBook-Pro.local>
2023-11-29 13:11:49 -0500

    fixing the compatibilty algo

--- src/classes/check.js
+++ src/classes/check.js
@@ -1,40 +1,43 @@
 export async function check_compatibility(classList, user){
-    const url = "https://4jui141iri.execute-api.us-east-1.amazonaws.com/dev/class/detailed"
-    const payload = {
-        username: user
-      };
-      
-      // Encode the payload
-      const params = new URLSearchParams(payload).toString();
-      
-      // Perform the GET request
-      fetch(`${url}?${params}`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
+  const url = "https://4jui141iri.execute-api.us-east-1.amazonaws.com/dev/class/detailed"
+  const payload = {
+      username: user
+    };
+    
+    // Encode the payload
+    const params = new URLSearchParams(payload).toString();
+    
+    // Perform the GET request
+    fetch(`${url}?${params}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Handle the response data
+        let coursesArray = JSON.parse(data.courses);
+        console.log(coursesArray);
+        for(const classInstance of classList.value){
+          for(const course of coursesArray){
+              if(!(classInstance.daysMeet == 'TR') != (course.days_meet == 'TR')){
+                  console.log(classInstance.daysMeet, course.days_meet )
+                  if(classInstance.startTime == course.start_time ||  classInstance.endTime == course.start_time || classInstance.startTime == course.end_time){
+                      classInstance.isCompatible = false;
+                      break;
+                  }else{
+                    classInstance.isCompatible = true;
+                  }
+              }else{
+                  console.log("initial condition wrong")
+                  classInstance.isCompatible = true;
+              }
+          }
         }
       })
-        .then(response => response.json())
-        .then(data => {
-          // Handle the response data
-          let coursesArray = JSON.parse(data.courses);
-          console.log(coursesArray);
-          for(const classInstance of classList.value){
-            for(const course of coursesArray){
-                if(!((classInstance.daysMeet == 'TR') !== (course.days_meet == 'TR'))){
-                    console.log(classInstance.daysMeet, course.days_meet )
-                    if(classInstance.startTime == course.start_time ||  classInstance.endTime == course.start_time || classInstance.startTime == course.end_time){
-                        classInstance.isCompatible = false;
-                        break;
-                    }
-                }else{
-                    classInstance.isCompatible = true;
-                }
-            }
-          }
-        })
-        .catch(error => {
-          // Handle errors
-          console.error('Error:', error);
-        });
+      .catch(error => {
+        // Handle errors
+        console.error('Error:', error);
+      });
 }
