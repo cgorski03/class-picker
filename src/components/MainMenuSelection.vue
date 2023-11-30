311f217977ab755efacbb287c805de109c16e991
Colin Gorski <colingorski@Colins-MacBook-Pro.local>
2023-11-29 22:50:17 -0500

    bug

--- src/components/MainMenuSelection.vue
+++ src/components/MainMenuSelection.vue
@@ -24,7 +24,7 @@ const navigate = () => {
     console.log("registering button ppress");
     logout({
         logoutParams: {
-            returnTo: 'http://localhost:5173/'
+            returnTo: window.location.origin
         },
     });
   }
