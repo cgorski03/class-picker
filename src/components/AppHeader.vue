b37207828647332f207ef8ce198300d2edb85f95
Archit Bhargava <archit@retailrealm.com>
2023-12-02 16:00:09 -0500

    Resolved merge conflicts

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,23 +9,7 @@
 <style scoped>
 /* Give the header itself a background color, a border, and add some padding to the content */
 .app-header {
-<<<<<<< HEAD
-  position: fixed; /* Make header stay at top */
-  top: 0;
-  left: 0;
-  right: 0;
-  background-color: #151E3D; /* Dark blue background */
-  color: white !important; /* White text color */
-  text-align: center;
-  padding: 1rem;
-  z-index: 1000; /* Ensure it's above other content */
-}
 
-.app-header h1 {
-  margin: 0; /* Remove default margin */
-  font-size: 1.6rem;
-  font-weight: bold;
-=======
   background-color: #2c3e50; /* Darker background color */
   border-bottom: 2px solid #34495e; /* Slightly darker border color */
   padding: 1rem; /* Adjusted padding */
@@ -40,7 +24,6 @@
   font-weight: bold;
   padding: 0.75rem; /* Adjusted padding */
   margin: 0; /* Removed default margin */
->>>>>>> 4c00aced35d8619dc42fc80038f7c97660101602
 }
 
 </style>
